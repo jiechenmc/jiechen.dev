0b957993bdd21710b8f013131ef5cc3f28db338d
Jie Chen <jiechenmc@gmail.com>
2024-05-03 22:42:45 -0400

    add swipe message

--- app/Projects/index.tsx
+++ app/Projects/index.tsx
@@ -18,24 +18,26 @@ const Projects = () => {
                             <ProjectEntries
                                 {...project}
                             />
-                            <Icon icon="mdi:gesture-swipe-horizontal" color="#00b3f0" className="absolute right-[5%] translate-y-4 lg:hidden md:w-12 md:h-12" width="24" height="24" />
                         </div>
                     ))}
                 </div>
 
-
                 <div className="flex justify-center w-full py-2 gap-2">
-                    {PROJECTS.map((a, index) =>
+                    {PROJECTS.map((_, index) =>
                         <a key={crypto.randomUUID()} href={`#item${index}`} className={`btn btn-xs ${index < PROJECTS.length / 2 ? "text-green-500" : "text-green-700"}`}>{index}</a>
                     )}
                 </div>
+                <div role="alert" className="alert w-fit mx-auto prose text-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>You can swipe if you have touch screen.</span>
+                </div>
             </div>
-            <div className="flex justify-center">
-                {/* <img
+            {/* <div className="flex justify-center">
+                <img
                     className="w-full md:w-[60%] lg:w-[45%] xl:w-[35%] 2xl:w-[25%]"
                     id="GithubStats"
-                /> */}
-            </div>
+                />
+            </div> */}
         </div >
     );
 };
