7591daa71b4e7edb316eab3fe981ef4110e844d3
Jie Chen <jiechenmc@gmail.com>
2024-05-06 16:20:00 -0400

    amazing

--- app/Hero/index.tsx
+++ app/Hero/index.tsx
@@ -27,14 +27,14 @@ const Hero = () => {
                             <ViewGithubButton />
                             <ViewBlogButton />
                         </div>
-                        <div role="alert" className="alert shadow-lg">
+                        <div role="alert" className="alert shadow-lg border-2 border-info">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                             <div>
                                 <h3 className="font-bold">New Blog Post!</h3>
                                 <div className="text-xs">Go Concurrency with Goroutines</div>
                             </div>
                             <Link href="https://blog.jiechen.dev/Go/Goroutines">
-                                <button className="btn btn-sm text-error">See</button>
+                                <button className="btn btn-sm text-error border-2 border-error hover:border-success hover:text-success">See</button>
                             </Link>
                         </div>
                     </div>
